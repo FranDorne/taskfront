61edfefb59d154587cf60e8f1156eaf9a381f4ef
FranDorne <francodornelesux@gmail.com>
2024-04-02 14:40:13 -0300

    first commmit

---
+++ src/api/tasks.ts
@@ -0,0 +1,31 @@
+import { CreateTask, UpdateTask } from "../interface/task.interface";
+
+const URL = import.meta.env.VITE_API || "http://localhost:3000"
+const API = `${URL}/api`;
+
+export const getTasksRequest = async () => fetch(`${API}/tasks`);
+
+export const createTaskRequest = async (task: CreateTask) =>
+  fetch(`${API}/tasks`, {
+    method: "POST",
+    body: JSON.stringify(task),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+export const deleteTaskRequest = async (id: string) =>
+  fetch(`${API}/tasks/${id}`, {
+    method: "DELETE",
+  });
+
+export const updateTaskRequest = async (id: string, task: UpdateTask) =>
+  fetch(`${API}/tasks/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(task),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+export const getTaskRequest = async (id: string) => fetch(`${API}/tasks/${id}`);
